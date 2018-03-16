f80774d1ae49725ceaebf046c48b89cdac3ff24d
Rich <richardwaynefedorajr@gmail.com>
2018-03-15 23:14:40 -0400

    Update interface bug (outputs not printing)

--- include/lib.h
+++ include/lib.h
@@ -6,14 +6,15 @@
 # include <tuple>
 
 // Generic classes for testing
-class A { public: virtual void get() = 0; };
-class A1 : public A { public: void get() { std::cout << "A1" << std::endl; } };
-class A2 : public A { public: void get() { std::cout << "A2" << std::endl; } };
+class A { public: virtual void print() = 0; };
+class A1 : public A { public: void print() { std::cout << "A1" << std::endl; } };
+class A2 : public A { public: void print() { std::cout << "A2" << std::endl; } };
+
+// Generic classes to test variadic abstract factory
+class V { public: int i; };
+class V1 : public V { public: V1(std::tuple<std::string,int> t) { i = std::get<1>(t); } };
 
 // Variadic abstract factory function (pass ctor arguments as tuple)
-class Avar { public: int i; };
-class A1var : public Avar { public: A1var(std::tuple<std::string,int> t) { i = std::get<1>(t); } };
-
 template<typename M, typename B, typename D, typename T> void f(M & m, T t) {
         m[std::get<0>(t)] = std::unique_ptr<B>( new D(t) );
 }
@@ -31,6 +32,56 @@ template <typename B, typename D1, typename D2> class AbstractFactory {
         std::unique_ptr<B> get2(X... args) { return std::move( std::unique_ptr<B>( new D2(args...) ) ); }
 };
 
+// General resource management interface (Singleton, interface, proxy, facade, creational elements)
+template<typename T, typename T1, typename T2> class Interface;
+template<typename T, typename T1, typename T2> class Singleton {
+   
+    friend class Interface<T,T1,T2>;
+    
+    public:
+        ~Singleton() = default;
+    private:
+        Singleton() { };
+        Singleton(const Singleton&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+        Singleton(Singleton&&) = delete;
+        Singleton& operator=(Singleton&&) = delete;
+
+        // Singleton instance
+        static std::unique_ptr<Singleton> m_instance;
+       
+        // Lazy initialization 
+        static Singleton& get() { 
+            if ( !m_instance ) { m_instance.reset(new Singleton); }
+            return *m_instance.get();
+        }
+
+        // Wrapper for creational functionality
+        void create() { m_T1 = m_factory.get1(); m_T2 = m_factory.get2(); }
+        
+        // Creational instance
+        AbstractFactory<T,T1,T2> m_factory;
+
+        // Resources to manage
+        std::unique_ptr<T> m_T1;
+        std::unique_ptr<T> m_T2;
+
+};
+
+// Singleton instance initialized to nullptr
+template<typename T, typename T1, typename T2>
+std::unique_ptr< Singleton<T,T1,T2> > Singleton<T,T1,T2>::m_instance = nullptr;
+
+// Declaration of interface class with attributes of proxy and facade design patterns
+template<typename T, typename T1, typename T2> class Interface {
+    public:
+        Interface() { m_Singleton = &Singleton<T,T1,T2>::get(); }
+        void set() { m_Singleton->create(); }
+        void print() { m_Singleton->m_T1->print(); m_Singleton->m_T2->print(); }
+    private:
+        Singleton<T,T1,T2>* m_Singleton;
+};
+
 // Builder
 template<typename T1, typename T2> class B {
     public:
@@ -46,47 +97,4 @@ template<typename T1, typename T2, typename C> class Builder {
         void build(T1 t1, T2 t2, C & c) { c.setT1(t1); c.setT2(t2); } 
         T1 m_T1;
         T2 m_T2;
-};
-
-// General resource management interface (Singleton, interface, proxy, facade elements)
-template<typename T, typename T1, typename T2> class Interface;
-template<typename T, typename T1, typename T2> class Singleton {
-   
-    friend class Interface<T,T1,T2>;
-    
-    public:
-        ~Singleton() = default;
-    private:
-        Singleton() { };
-        Singleton(const Singleton&) = delete;
-        Singleton& operator=(const Singleton&) = delete;
-        Singleton(Singleton&&) = delete;
-        Singleton& operator=(Singleton&&) = delete;
-
-        static std::unique_ptr<Singleton> m_instance;
-        
-        static Singleton& get() { 
-            if ( !m_instance ) { m_instance.reset(new Singleton); }
-            return *m_instance.get();
-        }
-
-        void create() { m_T1 = m_factory.get1(); m_T2 = m_factory.get2(); }
-        
-        AbstractFactory<T,T1,T2> m_factory;
-        std::unique_ptr<T> m_T1;
-        std::unique_ptr<T> m_T2;
-
-};
-
-template<typename T, typename T1, typename T2>
-std::unique_ptr< Singleton<T,T1,T2> > Singleton<T,T1,T2>::m_instance = nullptr;
-
-// Declaration of interface class with attributes of proxy and facade design patterns
-template<typename T, typename T1, typename T2> class Interface {
-    public:
-        Interface() { m_Singleton = &Singleton<T,T1,T2>::get(); }
-        void set() { m_Singleton->create(); }
-        void get() { m_Singleton->m_T1.get(); m_Singleton->m_T2.get(); }
-    private:
-        Singleton<T,T1,T2>* m_Singleton;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,43 @@ int main(int argc, char *argv[]) {
     // Show a rigid example of each creational design pattern with templates, but also show some interesting creational patterns which extend the generic ideology
 
     // Template can produce any combination of base and derived classes
+   
+    std::cout << "Abstract factory: " << std::endl;
+    
     AbstractFactory<A,A1,A2> factory;
     std::unique_ptr<A> a1 = factory.get1();
     std::unique_ptr<A> a2 = factory.get2();
-    a1->get();
-    a2->get();
+    a1->print();
+    a2->print();
+   
+    std::cout << std::endl;
 
     // Variadic template function can populate std::map with arbitrary number of derived classes accepting std::tuples for ctor arguments
-    std::map< std::string,std::unique_ptr<Avar> > m;
-    f< std::map<std::string,std::unique_ptr<Avar> >,Avar,A1var,A1var >(m,std::tuple<std::string,int>("Avar1:1",1),std::tuple<std::string,int>("Avar1:2",2));
+   
+    std::cout << "Variadic template factory: " << std::endl;
+   
+    std::map< std::string,std::unique_ptr<V> > m;
+    f< std::map<std::string,std::unique_ptr<V> >,V,V1,V1 >(m,std::tuple<std::string,int>("V1:1",1),std::tuple<std::string,int>("V1:2",2));
 
-    std::cout << "Avar1:1 " << m["Avar1:1"]->i << std::endl;
-    std::cout << "Avar1:2 " << m["Avar1:2"]->i << std::endl;
+    std::cout << "V1:1 " << m["V1:1"]->i << std::endl;
+    std::cout << "V1:2 " << m["V1:2"]->i << std::endl;
+    std::cout << std::endl;
 
+    // Singleton resource manager with interface using proxy and facade functionality
+    // DEBUG -> why aren't A1 and A2 printing
+   
+    std::cout << "Interface: " << std::endl;
+   
+    Interface<A,A1,A2> i = Interface<A,A1,A2>();
+    i.set();
+    i.print();
+    
+    std::cout << std::endl;
 
     // Builder can create objects with different member types
     // True builder pattern would have derived builders to apply different values for given templated type
+    std::cout << "Builder: " << std::endl;
+
     B<int,int> b1;
     B<std::string,std::string> b2;
     Builder< int,int, B<int,int> > builder1;
@@ -33,12 +54,8 @@ int main(int argc, char *argv[]) {
 
     b1.execute();
     b2.execute();
-
-    // Singleton resource manager with interface using proxy and facade functionality
-    // DEBUG -> why aren't A1 and A2 printing
-    Interface<A,A1,A2> i = Interface<A,A1,A2>();
-    i.set();
-    i.get();
+    
+    std::cout << std::endl;
 
     return 0;
 
